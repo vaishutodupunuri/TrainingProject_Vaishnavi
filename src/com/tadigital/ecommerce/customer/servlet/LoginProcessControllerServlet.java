ed06ca6b0cbfbffd1ce6e0ea57b9cffd5d2b1911
Vaishnavi Todupunuri <vaishutodupunuri@gmail.com>
2019-04-02 18:31:50 +0530

    LoginProcesscontrollerServlet java file added.

---
+++ src/com/tadigital/ecommerce/customer/servlet/LoginProcessControllerServlet.java
@@ -0,0 +1,49 @@
+package com.tadigital.ecommerce.customer.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.tadigital.ecommerce.customer.entity.Customer;
+
+import com.tadigital.ecommerce.customer.service.CustomerService;
+
+@WebServlet("/login")
+public class LoginProcessControllerServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("LoginForm.html");
+		rd.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		String password = req.getParameter("pwd");
+		
+		Customer customer = new Customer();
+		customer.setEmail(email);
+		customer.setPassword(password);
+		
+		CustomerService customerService = new CustomerService();
+		boolean status = customerService.loginCustomer(customer);	
+		if(status) {
+			HttpSession session = req.getSession();
+			session.setAttribute("CUSTOMERDATA", customer);
+			
+			RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.html");
+			rd.forward(req, resp);
+		} else {
+			HttpSession session = req.getSession();
+			session.setAttribute("status1", "false");
+			RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.jsp");
+			rd.forward(req, resp);
+			
+		}
+	}
+}
