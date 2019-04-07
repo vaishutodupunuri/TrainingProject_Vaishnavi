33cd22ca84dd450f76588037f2abc6c864c400d5
Vaishnavi Todupunuri <vaishutodupunuri@gmail.com>
2019-04-07 16:37:30 +0530

    Cutomer.java updated

--- src/com/tadigital/ecommerce/customer/entity/Customer.java
+++ src/com/tadigital/ecommerce/customer/entity/Customer.java
@@ -11,16 +11,31 @@ public class Customer {
 	
 	private String email;
 	private String password;
+	private String newpassword;
+	private String oldpassword;
+	private String reppassword;
+	
+	private String fn;
+	private String ln;
+	private String gen;
+	private String addr;
+	private String city;
+	private String zip;
+	private String state;
+	private String coun;
 	
 	public Customer() {
 	}
-	public Customer(int id, String firstName, String lastName, String email, String password) {
+	public Customer(int id, String firstName, String lastName, String email, String password,String oldp,String newp,String rep) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		
 		this.email = email;
 		this.password = password;
+		this.oldpassword=oldp;
+		this.newpassword=newp;
+		this.reppassword=rep;
 	}
 	
 	//GETTER OR ACCESSOR METHODS
@@ -40,6 +55,39 @@ public class Customer {
 	public String getPassword() {
 		return password;
 	}
+	public String getNewPassword() {
+		return newpassword;
+	}
+	public String getOldPassword() {
+		return oldpassword;
+	}
+	public String getRepPassword() {
+		return reppassword;
+	}
+	public String getFn() {
+		return fn;
+	}
+	public String getLn() {
+		return ln;
+	}
+	public String getGen() {
+		return gen;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public String getCity() {
+		return city;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public String getState() {
+		return state;
+	}
+	public String getCoun() {
+		return coun;
+	}
 	
 	//SETTER OR MUTATOR METHODS
 	public void setId(int id) {
@@ -57,6 +105,40 @@ public class Customer {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public void setNewPassword(String newp) {
+		this.newpassword = newp;
+	}
+	public void setOldPassword(String old) {
+		this.oldpassword = old;
+	}
+	public void setRepPassword(String rep) {
+		this.reppassword = rep;
+	}
+	public void setFn(String fn) {
+		this.fn = fn;
+	}
+	public void setLn(String ln) {
+		this.ln = ln;
+	}
+	public void setGen(String gen) {
+		this.gen = gen;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public void setCoun(String coun) {
+		this.coun = coun;
 	}
 	
 	@Override
